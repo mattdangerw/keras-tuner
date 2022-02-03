d643ad6fa19c2f4c79ce9663a00efe0f3653ef16
Matt Watson <mattdangerw@gmail.com>
2022-02-02 22:00:18 -0800

    Make script executable and runnable from any dir

--- shell/format.sh
+++ shell/format.sh
@@ -1,13 +1,15 @@
-isort --sl .
-black --line-length 85 .
+#!/bin/bash
 
-for i in $(find keras_tuner tests -name '*.py') # or whatever other pattern...
-do
-  if ! grep -q Copyright $i
-  then
+base_dir=$(dirname $(dirname $0))
+
+isort --sl ${base_dir}
+black --line-length 85 ${base_dir}
+
+for i in $(find ${base_dir} -name '*.py'); do
+  if ! grep -q Copyright $i; then
     echo $i
     cat shell/copyright.txt $i >$i.new && mv $i.new $i
   fi
 done
 
-flake8 .
+flake8 ${base_dir}
